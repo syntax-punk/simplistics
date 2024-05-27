b454cc737363a17dc6b9e6f81f7c66c0e99fb34f
syntax-punk <david.jaeren@gmail.com>
2024-05-27 17:10:09 +0200

    Figured out how to connect update db from express

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import {db} from './db';
+import {users} from './db/schema';
 
 dotenv.config();
 
@@ -9,8 +11,16 @@ app.use(cors());
 
 const port: number | string = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World');
+app.get('/', async (req: Request, res: Response) => {
+  const result = await db.insert(users).values({
+    id: '1',
+    name: 'John Stovner',
+    age: 30,
+    email: 'johnny.sto@test.com',
+  });
+
+  console.log('------>   ', result);
+  res.send('Hello World: ');
 });
 
 app.listen(port, () => {
